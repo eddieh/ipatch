d3395f7e642ce99059c09389060cea3351dbc2c3
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2015-03-17 10:03:08 -0700

    Initial Commit

---
+++ iPatch/iPatchPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  iPatchPlugIn.h
+//  iPatch
+//
+//  Created by Eddie Hillenbrand on 3/17/15.
+//
+//
+
+#import <Quartz/Quartz.h>
+
+@interface iPatchPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
